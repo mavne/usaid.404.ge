fcaaeb8b8dacb6b5700a243978875dae84bb838d
giorgi <giorgigvazava87@gmail.com>
2023-09-26 18:03:43 +0400

    edition & about done

---
+++ _markup/editions.php
@@ -0,0 +1,369 @@
+<?php 
+$version = "1.0.1";
+?>
+<!DOCTYPE html>
+<html lang="ka">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="/_markup/assets/img/favicon.svg" type="image/svg" sizes="32x32">
+	<title>USAID</title>
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/owl/owl.carousel.min.css">
+	<link rel="stylesheet" href="/_markup/assets/lightbox/jquery.lightbox.css">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/main.css?v=<?=$version?>">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/aos/aos.css">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/ge.css?v=<?=$version?>">
+	<link rel="stylesheet" type="text/css" href="/_markup/assets/css/responsive_ge.css?v=<?=$version?>">
+</head>
+<body class="news-page editions-page">
+
+<header>
+	<div class="top">
+		<div class="center">
+			<div class="logos-box">
+				<a href="" class="usaid">usaid</a>
+				<a href="" class="csogeorgia">csogeorgia</a>
+				<a href="" class="samoqalaqo">samoqalaqo</a>
+				<a href="" class="chaertemartvashi">chaertemartvashi</a>
+			</div>
+
+			<div class="right">
+				<div class="wrapper">
+					<button type="button" class="burger" id="myBurger" data-nav="closed">
+						<em>em</em>
+						<em>em</em>
+						<em>em</em>
+					</button>	
+
+					<div class="languages-box ka-active">
+						<a href="" class="az">AZ</a>
+						<a href="" class="ka">KA</a>
+					</div>
+
+					<form action="https://usaid.404.ge/_markup/" method="post" class="search-form" autocomplete="off">
+						<input type="text" name="keyword" class="keyword" value="">
+						<button type="submit" class="submit">submit</button>
+					</form>	
+				</div>
+			</div>
+
+			<div class="clearer"></div>
+
+			<div class="navigation-box" id="myNav">
+				<nav>
+					<ul>
+						<li><a href="">მთავარი გვერდი</a></li>
+						<li><a href="">ჩაერთე მართვაში</a></li>
+						<li><a href="">სიახლეები</a></li>
+						<li><a href="">გამოცემები</a></li>
+						<li><a href="">ვიდეო რესურსები</a></li>
+						<li><a href="">ჩვენ შესახებ</a></li>
+						<li><a href="">სასარგებლო ბმულები</a></li>
+						<li><a href="">კონტაქტი</a></li>
+					</ul>
+				</nav>
+
+				<div class="social-box">
+					<ul>
+						<li>
+							<a href="" class="facebook">facebook</a>
+						</li>
+
+						<li>
+							<a href="" class="youtube">youtube</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>		
+	</div>
+</header>
+
+<main>
+	<section class="all-news">
+		<div class="center">
+			<h1>გამოცემები</h1>
+
+			<div class="clearer"></div>
+
+			<div class="editions-box">
+				<div class="row">
+					<div class="col-md-4">
+						<a href="" class="edition">
+							<div class="year">2023</div>
+
+							<div class="image-box">
+								<div class="image" style="background-image:url('/_markup/assets/img/pdf.png');">
+									<img src="/_markup/assets/img/pdf.png" alt="" loading="lazy">
+								</div>
+							</div>
+
+							<h2>როგორ შეიძლებოდა შოვის ტრაგედიის თავიდან</h2>
+							
+							<div class="download">
+								<p>ჩამოტვირთვა</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4">
+						<a href="" class="edition">
+							<div class="year">2023</div>
+
+							<div class="image-box">
+								<div class="image" style="background-image:url('/_markup/assets/img/pdf.png');">
+									<img src="/_markup/assets/img/pdf.png" alt="" loading="lazy">
+								</div>
+							</div>
+
+							<h2>როგორ შეიძლებოდა შოვის ტრაგედიის თავიდან</h2>
+							
+							<div class="download">
+								<p>ჩამოტვირთვა</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4">
+						<a href="" class="edition">
+							<div class="year">2023</div>
+
+							<div class="image-box">
+								<div class="image" style="background-image:url('/_markup/assets/img/pdf.png');">
+									<img src="/_markup/assets/img/pdf.png" alt="" loading="lazy">
+								</div>
+							</div>
+
+							<h2>როგორ შეიძლებოდა შოვის ტრაგედიის თავიდან</h2>
+							
+							<div class="download">
+								<p>ჩამოტვირთვა</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4">
+						<a href="" class="edition">
+							<div class="year">2023</div>
+
+							<div class="image-box">
+								<div class="image" style="background-image:url('/_markup/assets/img/pdf.png');">
+									<img src="/_markup/assets/img/pdf.png" alt="" loading="lazy">
+								</div>
+							</div>
+
+							<h2>როგორ შეიძლებოდა შოვის ტრაგედიის თავიდან</h2>
+							
+							<div class="download">
+								<p>ჩამოტვირთვა</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4">
+						<a href="" class="edition">
+							<div class="year">2023</div>
+
+							<div class="image-box">
+								<div class="image" style="background-image:url('/_markup/assets/img/pdf.png');">
+									<img src="/_markup/assets/img/pdf.png" alt="" loading="lazy">
+								</div>
+							</div>
+
+							<h2>როგორ შეიძლებოდა შოვის ტრაგედიის თავიდან</h2>
+							
+							<div class="download">
+								<p>ჩამოტვირთვა</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4">
+						<a href="" class="edition">
+							<div class="year">2023</div>
+
+							<div class="image-box">
+								<div class="image" style="background-image:url('/_markup/assets/img/pdf.png');">
+									<img src="/_markup/assets/img/pdf.png" alt="" loading="lazy">
+								</div>
+							</div>
+
+							<h2>როგორ შეიძლებოდა შოვის ტრაგედიის თავიდან</h2>
+							
+							<div class="download">
+								<p>ჩამოტვირთვა</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4">
+						<a href="" class="edition">
+							<div class="year">2023</div>
+
+							<div class="image-box">
+								<div class="image" style="background-image:url('/_markup/assets/img/pdf.png');">
+									<img src="/_markup/assets/img/pdf.png" alt="" loading="lazy">
+								</div>
+							</div>
+
+							<h2>როგორ შეიძლებოდა შოვის ტრაგედიის თავიდან</h2>
+							
+							<div class="download">
+								<p>ჩამოტვირთვა</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4">
+						<a href="" class="edition">
+							<div class="year">2023</div>
+
+							<div class="image-box">
+								<div class="image" style="background-image:url('/_markup/assets/img/pdf.png');">
+									<img src="/_markup/assets/img/pdf.png" alt="" loading="lazy">
+								</div>
+							</div>
+
+							<h2>როგორ შეიძლებოდა შოვის ტრაგედიის თავიდან</h2>
+							
+							<div class="download">
+								<p>ჩამოტვირთვა</p>
+							</div>
+						</a>
+					</div>
+
+					<div class="col-md-4">
+						<a href="" class="edition">
+							<div class="year">2023</div>
+
+							<div class="image-box">
+								<div class="image" style="background-image:url('/_markup/assets/img/pdf.png');">
+									<img src="/_markup/assets/img/pdf.png" alt="" loading="lazy">
+								</div>
+							</div>
+
+							<h2>როგორ შეიძლებოდა შოვის ტრაგედიის თავიდან</h2>
+							
+							<div class="download">
+								<p>ჩამოტვირთვა</p>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="clearer"></div>
+
+			<div class="pagination">
+				<ul>
+					<li><a href="" class="active">1</a></li>
+					<li><a href="">2</a></li>
+					<li><a href="">3</a></li>
+					<li><a href="">4</a></li>
+					<li><a href="">5</a></li>
+				</ul>
+			</div>
+
+			<div class="clearer"></div>
+		</div>
+	</section>
+</main>
+
+<footer>
+	<div class="center">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="footer-nav">
+					<nav>
+						<ul>
+							<li><a href="">მთავარი</a></li>
+							<li><a href="">სიახლეები</a></li>
+							<li><a href="">გამოცემები</a></li>
+							<li><a href="">ვიდეო რესურსები</a></li>
+							<li><a href="">ჩვენ შესახებ</a></li>
+							<li><a href="">პროექტის შესახებ</a></li>
+							<li><a href="">კონტაქტი</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="middle">
+					<div class="wrapper">
+						<p>ვებ-გვერდი დამზადებულია სამოქალაქო საზოგადოების ინსტიტუტის მიერ USAID-ის ადგილობრივი თვითმმართველობის პროგრამის მხარდაჭერით</p>
+
+						<div class="logos">
+							<a href="" class="samoqalaqo">samoqalaqo</a>
+							<a href="" class="csogeorgia">csogeorgia</a>
+						</div>
+
+						<button type="button" class="e-bulletin"><span>ელ-ბიულეტენი</span></button>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="social">
+					<a href="" class="youtube">youtube</a>
+					<a href="" class="facebook">facebook</a>
+				</div>
+
+				<div class="contaxt-info">
+					<div class="wrapper">
+						<ul>
+							<li>
+								<em class="address">address</em>
+								<div class="data">
+									<h3>მისამართი</h3>
+									<p>საბეჭდი და ტიპოგრაფიულ ინდუსტრიის უშინაარსო</p>
+								</div>
+								<div class="clearer"></div>
+							</li>
+
+							<li>
+								<em class="phone">phone</em>
+								<div class="data">
+									<h3>ტელეფონი</h3>
+									<a href="">+995 555 12 34 56</a>
+								</div>
+								<div class="clearer"></div>
+							</li>
+
+							<li>
+								<em class="email">email</em>
+								<div class="data">
+									<h3>ელ.ფოსტა</h3>
+									<a href="">usaid@gmail.com</a>
+								</div>
+								<div class="clearer"></div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="copyright">
+		<div class="center">
+			<div class="wrapper">
+				<p>ყველა უფლება დაცულია ©2023</p>
+				<a href="" class="shindi">shindi</a>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+
+<script src="/_markup/assets/js/jquery.js"></script>
+<script src="/_markup/assets/lightbox/jquery.lightbox.js"></script>
+<script src="/_markup/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="/_markup/assets/js/owl.carousel.min.js"></script>
+<script src="/_markup/assets/js/compressed.js?v=<?=$version?>"></script>
+<script src="/_markup/assets/aos/aos.js"></script>
+</body>
+
+</html>
